00332c0b37b2b72189c1c53fa32e1f0ae94f2e07
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-05-23 11:30:49 +0530

    setting up project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const mongoose = require('mongoose') ;
+const dotenv = require('dotenv') ;
+const {app} = require('./app.js')
+// const connectDB = require('./config/db.js')
 
-sdaa
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = process.env.PORT || 5000
+
+
+app.get('/',(req,res)=>{
+    res.send('WE are working')    
+})
+
+app.listen(PORT,()=>{
+    console.log(`Its working on ${PORT}`)
+})
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+asyncHandler = () =>{
+    
+}
+
+module.exports = {
+    asyncHandler 
+}
