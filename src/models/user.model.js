08b914fe0cde0582fba7872b820bbbc7f2ee0d78
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-05-25 21:02:02 +0530

    user schema updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,45 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({}) ;
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is must"],
+    },
+    dp: {
+      type: String,
+    },
+    coverPhoto: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-export const User = mongoose.model('User' , userSchema) ;
+export const User = mongoose.model("User", userSchema);
