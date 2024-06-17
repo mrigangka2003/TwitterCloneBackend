29556b59455bd8deed257c54b917703799312e15
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-06-17 14:19:47 +0530

    controller and routes updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,12 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req,res) =>{
+    res.status(200).json({
+        message :"OK"
+    })
+})
+
+
+export {
+    registerUser
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./config/db.js";
 
+
 dotenv.config({
   path: "./.env",
 });
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +19,8 @@ connectDB()
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-  });
+});
+
 
 app.get("/", (req, res) => {
   res.send("WE are working");
