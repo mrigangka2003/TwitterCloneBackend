457a520194b1e657898ed40039b94af62d906c92
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-06-21 23:29:50 +0530

    updated user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,52 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/fileupload.js";
 
-const registerUser = asyncHandler(async (req,res) =>{
-    res.status(200).json({
-        message :"OK"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, fullname, email, password, bio} = req.body;
 
+  if (
+    [fullname, email, username, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-export {
-    registerUser
-}
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  //check the images
+  const dpLocalPath = req.files?.dp[0]?.path;
+  const coverLocalPath = req.files?.cover[0]?.path;
+
+  if (!dpLocalPath) {
+    throw new ApiError(400, " Dp file is required ");
+  }
+
+  //upload them to cloudinary
+  const dp = await uploadOnCloudinary(dpLocalPath);
+  const coverImage = await uploadOnCloudinary(coverLocalPath);
+
+  if (!dpLocalPath) {
+    throw new ApiError(400, " Dp file is required ");
+  }
+
+  User.create({
+    fullname,
+    dp: dp.url,
+    email,
+    coverImage: coverImage?.url || "",
+    password,
+    username: username.toLowerCase(),
+    bio:bio?.trim() || "" 
+  });
+});
+
+export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,22 @@
 import { registerUser } from "../controllers/user.controller.js";
 import {Router} from "express" ;
+
+
 const router = Router() ;
 
-router.route('/register').post(registerUser)
+import { upload } from "../middlewares/multer.middleware.js";
+
+router.route('/register').post(
+    upload.fields([
+        {
+            name : "dp" ,
+            maxCount : 1
+        },
+        {
+            name : "coverPhoto" ,
+            maxCount : 1
+        }
+    ])
+    ,registerUser)
 
 export default router ;
