bb17f43629ce3dda5b3d77b1e94da7b06910aef0
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-06-25 01:30:52 +0530

    logic up for logged In user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,8 +5,26 @@ import { uploadOnCloudinary } from "../utils/fileupload.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating referesh and access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, fullName, email, password, bio} = req.body;
+  const { username, fullName, email, password, bio } = req.body;
 
   if (
     [fullName, email, username, password].some(
@@ -27,11 +45,14 @@ const registerUser = asyncHandler(async (req, res) => {
   //check the images
   const dpLocalPath = req.files?.dp[0]?.path;
 
-  let coverPhotoLocalPath ;
-  if(req.files && Array.isArray(req.files.coverPhoto) && req.files.coverPhoto.length>0){
-    coverPhotoLocalPath = req.files.coverPhoto[0].path ;
+  let coverPhotoLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverPhoto) &&
+    req.files.coverPhoto.length > 0
+  ) {
+    coverPhotoLocalPath = req.files.coverPhoto[0].path;
   }
-
 
   if (!dpLocalPath) {
     throw new ApiError(400, " Dp file is required ");
@@ -53,22 +74,71 @@ const registerUser = asyncHandler(async (req, res) => {
     coverPhoto: coverPhoto?.url || "",
     password,
     username: username.toLowerCase(),
-    bio:bio?.trim() || "" 
+    bio: bio?.trim() || "",
   });
 
   const createdUser = await User.findById(user._id).select(
     "-password -refreshtoken"
-  )
+  );
 
-  if(!createdUser){
+  if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
 
-
-  return res.status(201).json(
-    new ApiResponse(200,createdUser,"User registered successfully")
-  )
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username && !email) {
+    throw new ApiError(400, "Username && email is required!");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(401, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
